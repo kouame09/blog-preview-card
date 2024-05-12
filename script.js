8bb5b2694fc4be679f67a3e35f4d508fe9339232
kouame09 <princekouame7@gmail.com>
2024-05-12 17:20:37 +0000

    commit one

---
+++ script.js
@@ -0,0 +1,8 @@
+//Ecris une fonction qui prend en entrée deux entier A et B et qui retourne la somme de leur carré
+
+function sommeCarre(A, B) {
+    return Math.pow(A, 2) + Math.pow(B, 2);
+}
+
+// Exemple d'utilisation
+console.log(sommeCarre(2, 3));
